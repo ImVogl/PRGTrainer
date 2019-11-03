3c36339c84d0acd8f228aa91b798e4ee1c317fca
Роман Кармацкий <Knispel.Kurt@gmail.com>
2019-11-03 05:36:54 +0300

    Добавлен контроллер состояний пользователей

---
+++ PRGTrainer.Core/Model/UserState.cs
@@ -0,0 +1,25 @@
+﻿namespace PRGTrainer.Core.Model
+{
+    using System;
+
+    /// <summary>
+    /// Плоский клас со сведениями о состоянии пользователя.
+    /// </summary>
+    public class UserState
+    {
+        /// <summary>
+        /// Идентификатор пользователя.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Состояние пользователя.
+        /// </summary>
+        public UserStates State { get; set; }
+
+        /// <summary>
+        /// Время последнего обновления состояния.
+        /// </summary>
+        public DateTime LastUpdateTime { get; set; }
+    }
+}
---
+++ PRGTrainer.Core/TelegramHandler/StatesController/ITestStateController.cs
@@ -0,0 +1,17 @@
+﻿namespace PRGTrainer.Core.TelegramHandler.StatesController
+{
+    using JetBrains.Annotations;
+
+    /// <summary>
+    /// Интерфейс контроллера состояний для тестов.
+    /// </summary>
+    public interface ITestStateController : IMessageProcessing
+    {
+        /// <summary>
+        /// Проверяет, проходит ли текущий пользователь тестирование.
+        /// </summary>
+        /// <param name="identifier">Идентификатор пользователя.</param>
+        /// <returns>Значение, показывающее, что пользователь проходит тестирование.</returns>
+        bool IsUserTakingTest([NotNull] int identifier);
+    }
+}
