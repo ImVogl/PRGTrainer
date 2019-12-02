e2e2db3fa1557880a9b1320b0ccdc27319cc054f
Роман Кармацкий <Knispel.Kurt@gmail.com>
2019-12-02 03:50:32 +0300

    Добавлен инструмент для модерации

---
+++ Source/PRGTrainer.Seperuser.Tool/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace PRGTrainer.SuperUser.Tool
+{
+    using System;
+    using System.Linq;
+    using TokenProcessing;
+
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            var tokenGenerator = new TokenGenerator();
+            if (args.Contains(@"-token"))
+                Console.WriteLine(
+                    tokenGenerator.Generate(
+                        args.Contains(@"-uStat") || args.Contains(@"-all"), 
+                        args.Contains(@"-qStat") || args.Contains(@"-all")));
+
+            Console.ReadKey();
+        }
+    }
+}
